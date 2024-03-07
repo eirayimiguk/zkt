99fd6d98b4bf49b625f61a0c43d20213530ec073
kgyuu <nakajima.k0301@gmail.com>
2024-03-07 23:56:46 +0900

    update

--- utils/zk-create.sh
+++ utils/zk-create.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+set -eu
+
+if [ -z "$ZK_ROOT" ]; then
+    echo "Error: ZK_ROOT environment variable is not set."
+    exit 1
+fi
 
 CURRENT_DATE=$(date +"%Y-%m-%d %H:%M:%S")
 UNIQUE_ID=$(echo $CURRENT_DATE | sed 's/-//g' | sed 's/ //g' | sed 's/://g')
-
-ZK_HOME="$HOME/zettelkasten"
-MD_FILE="$ZK_HOME/$UNIQUE_ID.md"
+MD_FILE="$ZK_ROOT/$UNIQUE_ID.md"
 
 echo "Create: $MD_FILE"
-
 cat note.tpl \
     | sed "s/\${uid}/${UNIQUE_ID}/g" \
     | sed "s/\${created_at}/${CURRENT_DATE}/g" \
     | sed "s/\${updated_at}/${CURRENT_DATE}/g" \
     > $MD_FILE
-
 nvim $MD_FILE
 
